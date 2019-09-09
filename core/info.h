be4800199eb4495718e5e1c47be7647c11b33a3d
Muki <michal@mkiol.net>
2019-09-09 20:26:06 +0200

    2.5.1 release

--- core/info.h
+++ core/info.h
@@ -11,9 +11,9 @@
 namespace Jupii {
 static const char* APP_NAME = "Jupii";
 #ifdef SAILFISH
-static const char* APP_VERSION = "2.5.0 (beta)";
+static const char* APP_VERSION = "2.5.1 (beta)";
 #elif DESKTOP
-static const char* APP_VERSION = "2.5.0 (beta)";
+static const char* APP_VERSION = "2.5.1 (beta)";
 #endif
 static const char* AUTHOR = "Michal Kosciesza";
 static const char* COPYRIGHT_YEAR = "2018-2019";
