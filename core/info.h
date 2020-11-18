5639ec09bb03d0bf5d122ca8da810f3e40947eea
Muki <michal@mkiol.net>
2020-11-18 17:54:51 +0100

    release 2.8.1

--- core/info.h
+++ core/info.h
@@ -13,9 +13,9 @@
 namespace Jupii {
 static constexpr const char* APP_NAME = "Jupii";
 #ifdef QT_DEBUG
-static constexpr const char* APP_VERSION = "2.8.0 (debug)";
+static constexpr const char* APP_VERSION = "2.8.1 (debug)";
 #else
-static constexpr const char* APP_VERSION = "2.8.0";
+static constexpr const char* APP_VERSION = "2.8.1";
 #endif // QT_DEBUG
 #ifdef SAILFISH
 static constexpr const char* APP_ID = "harbour-jupii";
